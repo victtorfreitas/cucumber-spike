ad5852d323e1e0045da2440987ae6b15db10b7df
victtor <victtorfreitas95@gmail.com>
2022-09-02 00:29:32 -0300

    Init project

---
+++ src/main/java/br/com/cucumberspike/IsItFriday.java
@@ -0,0 +1,10 @@
+package br.com.cucumberspike;
+
+public class IsItFriday {
+
+  public static String isIt(String today) {
+    return "Friday".equals(today) ? "TGIF" : "Nope";
+
+  }
+
+}
---
+++ src/test/java/br/com/cucumberspike/steps/ItsFridayStep.java
@@ -0,0 +1,37 @@
+package br.com.cucumberspike.steps;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import br.com.cucumberspike.IsItFriday;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ItsFridayStep {
+
+  private String today;
+  private String actualAnswer;
+
+  @Given("today is Sunday")
+  public void today_is_sunday() {
+    today = "Sunday";
+  }
+
+  @When("I ask whether it's Friday yet")
+  public void i_ask_whether_it_s_friday_yet() {
+    actualAnswer = IsItFriday.isIt(today);
+
+  }
+
+  @Then("I should be told {string}")
+  public void i_should_be_told(String expectedAnswer) {
+    assertEquals(expectedAnswer, actualAnswer);
+  }
+
+  @Given("today is Friday")
+  public void todayIsFriday() {
+    today = "Friday";
+  }
+
+}
